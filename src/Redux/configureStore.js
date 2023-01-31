f80b9268a2b47ed9fed68eba3d37042994986423
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-01-31 18:33:15 +0500

    Displayed rendered Rockets Section
    In this commit
    - Display the rendered rockets section only once.
    - Use useRef to stop re-rendering the fetched details of the rockets.
    - Style the rocket element and its components.

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,7 +1,8 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import roketsSlice from './Rockets/roketsSlice';
 
 const reducer = combineReducers({
-
+  rockets: roketsSlice,
 });
 
 const store = configureStore({
