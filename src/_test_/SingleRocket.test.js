2c173c42390ecf06b9d78d0a1e87337485322e11
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-02-01 18:44:52 +0500

    Add test for SingleRocket component.

---
+++ src/_test_/SingleRocket.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from '../Redux/configureStore';
+import SingleRocket from '../components/singleRocket';
+import { render } from '@testing-library/react';
+import Rockets from '../pages/Rockets';
+
+
+describe('It renders', () => {
+  it('SingleRocket correctly', () => {
+    const tree = render(
+      <Provider store = {store}>
+        <SingleRocket />
+      </Provider>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Rockets correctly', () => {
+    const tree = render(
+      <Provider store = {store}>
+        <Rockets />
+      </Provider>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
